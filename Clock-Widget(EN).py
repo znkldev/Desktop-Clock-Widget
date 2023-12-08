3d358687c8bf38be9dc5be0a6b249ebf4b5dccc4
Selim <muhammedselzink@outlook.com>
2023-12-08 21:48:52 +0300

    v1.0.1
    Window Transparency: Utilizing Win32 API to make the window's background transparent.
    Real-Time Clock and Date Display: Functions to dynamically update and display current time and date.
    Toggle Window Visibility: Using a keyboard shortcut (Win+H) to toggle the window between visible and hidden states.
    Window Management: Configuring Tkinter window settings such as removing the frame, setting background color, fixing the size, and keeping it always on top.

--- Clock-Widget(EN).py
+++ Clock-Widget(EN).py
@@ -1,40 +1,48 @@
 from tkinter import *
 from time import strftime
-
 import win32gui
 import win32con
+import keyboard
 
-root = Tk() # Creating a Tkinter window
-root.title('Clock - Widget Python') # Setting tkinter window title (only applicable in Windows window management)
-screen_height = root.winfo_screenheight() # Get screen height
-screen_width = root.winfo_screenwidth() # Set window width and height, place it at the bottom left corner
-root.geometry("110x75+{}+{}".format(screen_width - 110, screen_height - 115)) 
-root.overrideredirect(True) # Removing window frame
-root.configure(background="black") # Setting background color
-root.resizable(0, 0) # Making the window size fixed
-root.wm_attributes('-topmost', True) # Setting the window to always stay on top
-root.grab_set() # Redirecting keyboard and mouse inputs to this window
-root.lift() # Setting the window to stay in front of other windows
-root.attributes("-toolwindow", 1) # Removing window title and frame
-root.attributes("-alpha", 0.50) # Setting window transparency
+# Creating a Tkinter window
+root = Tk()
+root.title('Clock - Widget Python')  # Setting the title of the tkinter window
 
-hwnd = win32gui.GetForegroundWindow() # Get the current active window
+# Getting screen height and width, setting window size, positioning it at the bottom left corner
+screen_height = root.winfo_screenheight()
+screen_width = root.winfo_screenwidth()
+root.geometry("110x75+{}+{}".format(screen_width - 110, screen_height - 115))
+
+# Removing window frame and making the window transparent
+root.overrideredirect(True)
+root.configure(background="black")  # Setting the background color
+root.resizable(0, 0)  # Making the window size fixed
+root.wm_attributes('-topmost', True)  # Setting the window to always stay on top
+root.grab_set()  # Redirecting keyboard and mouse inputs to this window
+root.lift()  # Setting the window to stay in front of other windows
+root.attributes("-toolwindow", 1)  # Removing window title and frame
+root.attributes("-alpha", 0.50)  # Setting window transparency
+
+# Making the window transparent and click-through using Win32 API
+hwnd = win32gui.GetForegroundWindow()
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
-                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_TRANSPARENT) # Set window transparency
+                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_TRANSPARENT)
 
 def clock():
     text = strftime("%H:%M")
-    date_text = strftime("%d/%m/%Y")  # Set the date format (day/month/year)
+    date_text = strftime("%d/%m/%Y")  # Setting the date format (day/month/year)
     time_label.config(text=text)
     date_label.config(text=date_text)
     time_label.after(1000, clock)
 
-# Function to close the window
-def close_window():
-    root.destroy()
+def toggle_window_visibility():
+    if root.state() == "withdrawn":  # If the window is hidden
+        root.deiconify()  # Show the window
+    else:
+        root.withdraw()  # Hide the window
 
-# To close after 10 seconds
-# root.after(10000, close_window)
+# Listen for Win+H key combination to show/hide the window
+keyboard.add_hotkey('win+h', toggle_window_visibility)
 
 time_label = Label(root, font=("Montserrat", 23, "bold"), background="black", foreground="white", anchor="n")
 time_label.pack(anchor="nw", padx=10, pady=0)
